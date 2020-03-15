ad20ce029a83edb99a86e698e0df90596f960589
Nathalia <nathalia.alvarez@globant.com>
2020-03-14 23:35:37 -0500

    screenshots

--- Cypress/cypress/integration/simple.spec.js
+++ Cypress/cypress/integration/simple.spec.js
@@ -2,16 +2,17 @@
 /*Login, búsqueda de profesores y filtros por materia en la página de un profesor*/
 describe('Los estudiantes login', function () {
   it('Visits los estudiantes and pass at login', function () {
-    cy.visit('https://losestudiantes.co')
-    cy.screenshot()
+    cy.visit('https://losestudiantes.co')    
     cy.contains('Cerrar').click()
-    //Lineas nuevas  
+    //Lineas nuevas
+    cy.screenshot()
     cy.contains('Ingresar').click()
     cy.get('.cajaLogIn').find('input[name="correo"]').click().type("jmauriciopv@gmail.com")
     cy.get('.cajaLogIn').find('input[name="password"]').click().type("01010101")
-    cy.get('.cajaLogIn').contains('Ingresar').click()
-    cy.screenshot()
+    cy.get('.cajaLogIn').contains('Ingresar').click() 
+    cy.wait(2000)   
     cy.contains('Busca y califica profesores y clases anónimamente')
+    cy.screenshot()
   })
 });
 
@@ -55,9 +56,8 @@ describe('Los estudiantes Registro con cuenta existente', function () {
 describe('Los estudiantes Busqueda Profesores', function () {
   it('Visits los estudiantes and search a teacher', function () {
     cy.visit('https://losestudiantes.co')
-    cy.screenshot()
     cy.contains('Cerrar').click()
-
+    cy.screenshot()
     cy.get('div[class="Select-placeholder"]').click()
     cy.get('input').type("Mario Linares")
     cy.get('div[id="react-select-required_error_checksum--option-0"]').contains("Mario Linares")
@@ -68,12 +68,11 @@ describe('Los estudiantes Busqueda Profesores', function () {
 describe('Los estudiantes Dirigirse a Profesor', function () {
   it('Visits los estudiantes and go to a teacher page', function () {
     cy.visit('https://losestudiantes.co')
-    cy.screenshot()
     cy.contains('Cerrar').click()
-
-    cy.get('div[class="Select-placeholder"]').click()
-    cy.get('input').type("Pruebas Automaticas")
-    cy.get('div[id="react-select-required_error_checksum--option-0"]').contains("MISO4208")
+    cy.screenshot()
+    cy.get('.buscador').find('div[class="Select-placeholder"]').click().type("Mario Linares")
+    cy.get('.buscador').find('div[id="react-select-required_error_checksum--option-0"]').click()
+    cy.get('.columnLeft').contains("Mario Linares")
     cy.screenshot()
   })
 });
